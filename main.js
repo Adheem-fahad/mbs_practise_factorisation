1cc74e5b07cfa2b191a101fce470a06ce520d49c
Adheem-fahad <141425028+Adheem-fahad@users.noreply.github.com>
2024-06-23 09:00:11 +0000

    js

---
+++ main.js
@@ -0,0 +1,7 @@
+let inputBox = document.querySelector('.inputt');
+let submitBtn = document.querySelector('.buttonn');
+
+submitBtn.onclick = () => {
+    const valueOfPolynomial = inputBox.value;
+    console.log(valueOfPolynomial)
+}
